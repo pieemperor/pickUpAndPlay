4a9c5ef4cc3f59cc35fcb3f8c15ccff9d097af85
Dakota Cowell <dakotacowell@gmail.com>
2017-09-05 11:29:29 -0400

    Added question mark icon

---
+++ pickUpAndPlay/WebSiteViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WebSiteViewController.swift
+//  pickUpAndPlay
+//
+//  Created by Dakota Cowell on 9/5/17.
+//  Copyright © 2017 Caleb Mitcler. All rights reserved.
+//
+
+import UIKit
+
+class WebSiteViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = URL(string: "https://pickupandplay-67953.firebaseapp.com/index.html") {
+            let request = URLRequest(url: url)
+            webView.loadRequest(request)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
